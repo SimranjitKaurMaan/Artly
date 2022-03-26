3f64d5863e7411b81d5142f1985b8da3c4db8ebe
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-03-26 18:24:26 +0530

    Removed assets, using only cloudinary

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -27,4 +27,32 @@ export const categories = [
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
     artworksCount: 16 
   },
+  {
+    _id: uuid(),
+    categoryName: "Oil Paintings",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    artworksCount: 8
+  },
+  {
+    _id: uuid(),
+    categoryName: "Basel Works",
+    description:
+      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    artworksCount: 20  
+  },
+  {
+    _id: uuid(),
+    categoryName: "Water",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    artworksCount: 16 
+  },  
+  {
+    _id: uuid(),
+    categoryName: "Portraits",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    artworksCount: 16 
+  }, 
 ];
