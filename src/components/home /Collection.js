2fef2cf582d4c469504bfea470b1e5f15d580565
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-03-26 18:04:37 +0530

    Corrected collections

--- src/components/home /Collection.js
+++ src/components/home /Collection.js
@@ -3,23 +3,28 @@ import { fetchCategories, fetchProductsImageUrlByCategory } from "../utils/reque
 
 export const Collection = () => {
     const [categories , setCategories] = useState([]);
+    const [categoryProducts, setCategoryProducts] = useState({});
     useEffect(() => {
         (async () => {
         const categories = await fetchCategories();
         setCategories(categories);
+        categories.map(async (category) => {
+            const products = await fetchProductsImageUrlByCategory(category.categoryName);
+            setCategoryProducts(c => ({...c, [category.categoryName]: products}));
+        })
         })();
     },[])
     return (
     <div>
         <div className="collections-header">Collections</div>
         <div className="collections-container flex-row-wrap-center">
-            { categories && categories.map(category =>
+            { Object.keys(categoryProducts).length === categories.length && categories.map(category =>
             <div className="collection flex-col-wrap-start">
                 <div className="grid-4">
-                    <a href="./products-vertical.html"><img className="collection-left-top-img img-sm" src={category.imageUrl} alt="artworks"/></a> 
-                    <a href="./products-vertical.html"><img className="collection-right-top-img img-sm" src={category.imageUrl} alt="artworks"/></a>
-                    <a href="./products-vertical.html"><img className="collection-left-bottom-img img-sm" src={category.imageUrl} alt="artworks"/></a>
-                    <a href="./products-vertical.html"><img className="collection-right-bottom-img img-sm" src={category.imageUrl} alt="artworks"/></a>
+                    <a href="./products-vertical.html"><img className="collection-left-top-img img-sm" src={categoryProducts[category.categoryName][0]} alt="artworks"/></a> 
+                    <a href="./products-vertical.html"><img className="collection-right-top-img img-sm" src={categoryProducts[category.categoryName][1]} alt="artworks"/></a>
+                    <a href="./products-vertical.html"><img className="collection-left-bottom-img img-sm" src={categoryProducts[category.categoryName][2]} alt="artworks"/></a>
+                    <a href="./products-vertical.html"><img className="collection-right-bottom-img img-sm" src={categoryProducts[category.categoryName][3]} alt="artworks"/></a>
                 </div>
                 <div className="collection-header">{category.categoryName}</div>
                 <div className="collection-subheader">{category.artworksCount} Artworks</div>
