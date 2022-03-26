94dc5c89d02401a11b7169c1e82e2a96de5f9e89
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-03-26 18:20:50 +0530

    Removed image urls

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -11,23 +11,20 @@ export const categories = [
     categoryName: "Photography",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
-    artworksCount: 12,
-    imageUrl: "https://res.cloudinary.com/duddwta8d/image/upload/v1648081716/collection-2_knhw2x.jpg"
+    artworksCount: 12
   },
   {
     _id: uuid(),
     categoryName: "Impressionism",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
-    artworksCount: 20,
-    imageUrl: "https://res.cloudinary.com/duddwta8d/image/upload/v1648081718/portrait-5_nrwqhi.jpg"   
+    artworksCount: 20  
   },
   {
     _id: uuid(),
     categoryName: "PopArt",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
-    artworksCount: 16,
-    imageUrl: "https://res.cloudinary.com/duddwta8d/image/upload/v1648081719/portrait-7_kymgrl.jpg"   
+    artworksCount: 16 
   },
 ];
