2615982c3681406998fb7908826bdca010cdc53c
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-03-27 11:02:00 +0530

    revert the product context

--- src/contexts/product-context.js
+++ src/contexts/product-context.js
@@ -1,24 +1,24 @@
-import { createContext, useContext, useReducer, useState, useEffect } from "react";
+// import { createContext, useContext, useReducer, useState, useEffect } from "react";
 
-const ProductContext = createContext();
+// const ProductContext = createContext();
 
-export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        (async () => {
-        const products = await fetchProductsByCategory(category);
-        setProducts(products);
-        })();
-    },[])
-    const [state, dispatch] = useReducer(productReducer, {
-        products: categoryProducts,
-        filteredProducts: categoryProducts
-    });
-    return (
-        <ProductContext.Provider value={{ state, dispatch}}>
-            {children}
-        </ProductContext.Provider>
-    );
-};
+// export const ProductProvider = ({children}) => {
+//     const [products, setProducts] = useState([]);
+//     useEffect(() => {
+//         (async () => {
+//         const products = await fetchProductsByCategory(category);
+//         setProducts(products);
+//         })();
+//     },[])
+//     const [state, dispatch] = useReducer(productReducer, {
+//         products: categoryProducts,
+//         filteredProducts: categoryProducts
+//     });
+//     return (
+//         <ProductContext.Provider value={{ state, dispatch}}>
+//             {children}
+//         </ProductContext.Provider>
+//     );
+// };
 
-export const useFilteredProducts = () => useContext(ProductContext);
+// export const useFilteredProducts = () => useContext(ProductContext);
