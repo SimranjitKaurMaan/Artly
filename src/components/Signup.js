bb5741d1e9ea886a02a93ecf2d1a0a25643680d8
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-03-27 11:24:58 +0530

    Added Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,3 +1,28 @@
+import {Link} from 'react-router-dom';
+
 export const SignUp = () => {
-    return <div>SignUp</div>
+    return (<main>
+    <div className="main-section">
+        <div className="main-container flex-col-wrap-center">
+            <div className="sign-up card-container">
+                <div className="input-container flex-col-wrap-center">
+                    <h2>Signup</h2>
+                    <div className="input-group">
+                        <div className="input-label">Email address</div>
+                        <input type="text" className="input-field input-field-20w" placeholder="abc@gmail.com"></input>
+                    </div>
+                    <div className="input-group">
+                        <div className="input-label">Password</div>
+                        <input type="password" className="input-field input-field-20w" placeholder="************"></input>
+                    </div>
+                    <div className="input-group">
+                        <input type="radio" className="input-field"></input><span className="radio-text">I accept all Terms & Conditions</span>
+                    </div>
+                    <button className="btn btn-primary">Create New Account</button>
+                    <Link to="./login">Already have an account<i className="fa fa-thin fa-chevron-right"></i></Link>
+                </div>
+            </div>
+        </div>
+    </div>
+  </main>)
 }
